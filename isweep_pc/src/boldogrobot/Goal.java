341fb32da93685bfae9292336f8b302fc21afc79
user <user@debian.compute.dtu.dk>
2014-03-19 21:16:46 +0100

    more

---
+++ isweep_pc/src/boldogrobot/Goal.java
@@ -0,0 +1,9 @@
+package boldogrobot;
+
+public class Goal {
+	
+String  goal1;
+String goal2;
+
+
+}
---
+++ isweep_pc/src/boldogrobot/Point.java
@@ -0,0 +1,18 @@
+package boldogrobot;
+
+public class Point {
+
+	int x;
+	int y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+
+	}
+	public void  Distance(int x , int y) {
+
+	//	Math.abs(x);
+		
+	}
+}
