c404a759c6d18bdfddd851d8ea977323d0d13929
user <user@debian.compute.dtu.dk>
2014-03-19 21:15:54 +0100

    Dijkstra er implementeret

--- isweep_pc/src/boldogrobot/Bold.java
+++ isweep_pc/src/boldogrobot/Bold.java
@@ -1,27 +1,8 @@
 package boldogrobot;
 
-
 public class Bold {
-	private static final int BANE_W = 180;
-	private static final int BANE_H = 120;
+	public void tegn(int x, int y, char symbol) {
 
-	class Bane {
-		private char[][] pixels;
-
-		public Bane(int w, int h) {
-			this.pixels = new char[w][h];
-
-			for (int i = 0; i < pixels.length; i++) {
-				for (int j = 0; j < pixels[i].length; j++) {
-					pixels[i][j] = '-';
-				}
-			}
-		}
-
-		public void tegn(int x, int y, char symbol) {
-			pixels[x][y] = symbol;
-
-		}
 	}
 
 }
