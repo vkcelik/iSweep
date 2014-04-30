ebf1dd2e2a5f3133cf0e1e1d2c8c1e54d57d8b47
ziadbahlawan <ziad.93@hotmail.com>
2014-04-30 12:45:40 +0200

    lidt mere distance

--- isweep_pc/src/control/Movement.java
+++ isweep_pc/src/control/Movement.java
@@ -1,18 +1,16 @@
-package nxt;
-
+package control;
 
 import lejos.nxt.Motor;
 
 public class Movement {
 	
-	
 	double convert = 512.0/180.0;
+	
 
-	void move(int degress){
+	void move(int millimeter){
 		Motor.A.rotate(600,true); 
 		Motor.B.rotate(600,false);
 	}
-	
 	
 	void armCollect(){
 		Motor.C.setSpeed(200);
@@ -29,16 +27,9 @@ public class Movement {
 		Motor.C.rotateTo(0);
 	
 	}	
+
 	void turnDirection(int angle){
 		Motor.A.rotate((int)(angle*convert),true);
 		Motor.B.rotate((int)(-angle*convert),false);
 	}
-	
 }
-	
-
-
-
-	
-	
-
