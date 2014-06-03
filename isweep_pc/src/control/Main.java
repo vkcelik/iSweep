622350eee7b587ecf9a1ed39331981d373a4af94
Assaadkassem <Assaadkassem@Kassem>
2014-06-03 14:54:11 +0200

    fra assaad

--- isweep_pc/src/control/Main.java
+++ isweep_pc/src/control/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		Robot robot = new Robot(1704, 713);
-		robot.setBack(new Placeable(1673, 514));
+		Robot robot = new Robot(703, 631);
+		robot.setBack(new Placeable(710, 527));
 		robot.setDirection(new Direction(robot.getX()-robot.getBack().getX(),robot.getY()-robot.getBack().getY()));
 		System.out.println(robot.getDirection());
 		
-		Ball b1 = new Ball(795,979);
-		
+		Ball b1 = new Ball(1681,807);
+	
 		Direction d = new Direction(b1.getX() - robot.getX(), b1.getY() - robot.getY());
 		System.out.println(d);
 		
@@ -27,19 +27,25 @@ public class Main {
 				/ ((Math.sqrt(Math.pow(robote0, 2) + (Math.pow(robote1, 2))) * (Math
 						.sqrt(Math.pow(toBalle0, 2) + (Math.pow(toBalle1, 2))))));
 		
-		
+		double vinkel_grader = Math.toDegrees(Math.acos(vinkel));
 		
 		Movement m = new Movement();
-		m.turnDirection((int)vinkel);
+//		if(robot.getX()>b1.getX()){
+//			m.turnRight((int)vinkel_grader);			
+//		} else {
+//			m.turnRight((int)vinkel_grader);
+//		}
 		
-		System.out.println(Math.toDegrees(Math.acos(vinkel)));
+		System.out.println(vinkel_grader);
 
-			int L_side_PX=1078, L_side_MM=1200;
+			int L_side_PX=1014, L_side_MM=1200;
 			float pixelpermm = (float)L_side_PX/L_side_MM;
 			int distance_mm = (int) (robot.getDistance(b1)/pixelpermm);
 		
 		System.out.println(distance_mm);
-		m.move(distance_mm);
+//		m.move(distance_mm);
+m.move(1500);
+
 
 //		Movement m = new Movement();
 
