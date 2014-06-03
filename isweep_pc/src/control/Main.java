8eed8adf920b0851b5baafd2798841e5bcc3af24
Assaadkassem <Assaadkassem@Kassem>
2014-06-03 16:31:17 +0200

    tam iste

--- isweep_pc/src/control/Main.java
+++ isweep_pc/src/control/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		Robot robot = new Robot(703, 631);
-		robot.setBack(new Placeable(710, 527));
+		Robot robot = new Robot(1530, 412);
+		robot.setBack(new Placeable(1541, 505));
 		robot.setDirection(new Direction(robot.getX()-robot.getBack().getX(),robot.getY()-robot.getBack().getY()));
 		System.out.println(robot.getDirection());
 		
-		Ball b1 = new Ball(1681,807);
-	
-		Direction d = new Direction(b1.getX() - robot.getX(), b1.getY() - robot.getY());
+		Ball b1 = new Ball(825,803);
+		Ball b1_aligned_arm =  new Ball(b1.getX()+127, b1.getY()+60);
+		Direction d = new Direction(b1_aligned_arm.getX() - robot.getX(), b1_aligned_arm.getY() - robot.getY());
 		System.out.println(d);
 		
 		int robote0 = robot.getDirection().getElement(0);
@@ -30,28 +30,23 @@ public class Main {
 		double vinkel_grader = Math.toDegrees(Math.acos(vinkel));
 		
 		Movement m = new Movement();
-//		if(robot.getX()>b1.getX()){
-//			m.turnRight((int)vinkel_grader);			
-//		} else {
-//			m.turnRight((int)vinkel_grader);
-//		}
+		if(robot.getX()>b1.getX()){
+			m.turnLeft((int)vinkel_grader);			
+		} else {
+			m.turnRight((int)vinkel_grader);
+		}
 		
 		System.out.println(vinkel_grader);
 
-			int L_side_PX=1014, L_side_MM=1200;
+			int L_side_PX=1000, L_side_MM=1200;
 			float pixelpermm = (float)L_side_PX/L_side_MM;
 			int distance_mm = (int) (robot.getDistance(b1)/pixelpermm);
 		
 		System.out.println(distance_mm);
-//		m.move(distance_mm);
-m.move(1500);
+		m.move(distance_mm);
 
-
-//		Movement m = new Movement();
-
-		// m.move(600);
-		// m.armCollect();
-		// m.armHold();
+		 m.armCollect();
+		 m.armHold();
 		// m.turnDirection(180);
 		// m.move(600);
 
